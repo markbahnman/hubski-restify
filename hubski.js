db2ebc745bff267556091bf39d26e20d20b1e55e
Mark Bahnman <mark.bahnman@gmail.com>
2013-04-09 02:41:09 -0700

    Added post GETing functionality

--- hubski.js
+++ hubski.js
@@ -1,17 +1,47 @@
 var $ = require('jquery');
 
-var getFeed = function(username) {
+// Constants which may be subject to change with mk's whims
+var BASEURL = 'http://hubski.com/';
+var FEEDURL = 'feed?id=';
+var POSTURL = 'pub?id=';
 
+/*
+ * General purpose async GET function. It provide us with
+ * most everything we need for this api.
+ */
+var GET = function(URL, parser) {
 	var dfd = $.Deferred();
 
 	var requestHTML = $.ajax({
 		type: "GET",
-		url: 'http://hubski.com/feed?id=' + username,
+		url: URL,
 		datatype: 'html',
 		success: function(data) {
-			var feedJSON = parseFeedJSON(data);
-			dfd.resolve(feedJSON);
+			var parsedData = parser(data);
+			dfd.resolve(parsedData);
 		}
+	});
+	return dfd.promise();
+}
+
+var getPost = function(id) {
+	var dfd = $.Deferred();
+	GET(BASEURL+POSTURL+id,parsePost).done(function(data) {
+		dfd.resolve(data);
+	});
+
+	return dfd.promise();
+}
+
+exports.getPost = getPost;
+
+var getFeed = function(username) {
+	var URL = BASEURL+FEEDURL+username;
+
+	var dfd = $.Deferred();
+
+	GET(URL, parseFeed).done(function(data) {
+		dfd.resolve(data);
 	});
 
 	return dfd.promise();
@@ -19,10 +49,41 @@ var getFeed = function(username) {
 
 exports.getFeed = getFeed;
 
-var parseFeedJSON = function(html) {
-	var baseURL = 'http://hubski.com/';
-	// Setup our posts array which will contain all of the post objects
+var parsePost = function(html) {
+	$doc = $(html);
+	var title = $doc.find('title').html();
+	var link = $doc.find('span.title > a').attr('href');
+	var text = $doc.find('div.pubcontent > div.pubtext').html();
+	var tags = getTags($doc.find('div.subtitle').html());
+	var sharedBy = $doc.find('div.subtitle > span > a.ajax').html();
+	var author = removeTagFromString($doc.find('div.subtitle > a').html(), 'font');
+	var posted = getTime(html);
+	var numOfComments = removeTagFromString($doc.find('span.combub > a').html(),'img');
+	var comments = getComments(html);
+	var JSON = {'Title': title, 'Link': link, 'text': text, 'tags': tags, 'Posted': posted, 'Author': author, 'SharedBy': sharedBy, 'NumberOfComments': numOfComments, 'comments': comments};
+	return JSON;
+}
 
+var getTags = function(tagString) {
+	var tags = [];
+	//TODO: Use some regex to get the tags
+	return tags;
+}
+
+var getComments = function(html) {
+	$doc = $(html);
+	var comments = [];
+	//TODO: Create an array of comments and their relationships
+	return comments;
+}
+
+var getTime = function(html) {
+	$doc = $(html);
+	var posted = null;
+	//TODO: Parse out the time of the post
+	return posted;
+}
+var parseFeed = function(html) {
 	var posts = [];
 	$doc = $(html);
 
@@ -40,8 +101,8 @@ var parseFeedJSON = function(html) {
 		 */
 		var voteLink = node.find('.plusminus > a').attr('href');
 		var topCommentor = node.find('.topcommentor > a').html();
-		var topCommentorLink = baseURL + node.find('.topcommentor > a').attr('href');
-		var commentsLink = baseURL + node.find('.feedcombub > a').attr('href');	
+		var topCommentorLink = BASEURL + node.find('.topcommentor > a').attr('href');
+		var commentsLink = BASEURL + node.find('.feedcombub > a').attr('href');	
 		
 
 		/* 
@@ -79,7 +140,7 @@ var parseFeedJSON = function(html) {
 	return feed;
 }
 
-exports.parseFeedJSON = parseFeedJSON;
+exports.parseFeed = parseFeed;
 
 function removeTagFromString(string, tag) {
 	var re = new RegExp('<'+tag+'.*/>','g');
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 var restify = require('restify');
 var hubski = require('./hubski.js');
 
-function respond(req, res, next) {
+function feed(req, res, next) {
 	hubski.getFeed(req.params.username).done(function(data) {
         res.send(data);
     });
+}
 
+function post(req, res, next) {
+    hubski.getPost(req.params.id).done(function(data) {
+        res.send(data);
+    });
 }
 
 var server = restify.createServer();
 server.pre(restify.pre.userAgentConnection());
-server.get('/feed/:username', respond);
-server.head('/feed/:username', respond);
+server.get('/feed/:username', feed);
+server.get('/post/:id', post);
 
 var port = process.env.PORT || 5000
 server.listen(port, function() {
